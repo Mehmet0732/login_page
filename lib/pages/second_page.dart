b8d4545f83b42c932cb32614c419871d99951b7f
Mehmet KARA <85625729+Mehmet0732@users.noreply.github.com>
2023-08-09 14:34:20 +0300

    Update second_page.dart
    Second sayfada bottom güncellemeleri yapılmıştır.

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:login_page_getx_examp/pages/user_controller.dart';
-
+import 'package:login_page_getx_examp/pages/third_page.dart'; // Üçüncü sayfa import edildi
 import 'package:login_page_getx_examp/controllers/todo_controller.dart'; //Yapılacaklar listesi işlemlerini yönetmek için TodoController sınıfını içeri aktarıyorsunuz
 import 'package:login_page_getx_examp/models/todo.dart'; //Yapılacaklar listesi için Todo modelini içeri aktarıyorsunuz
 
@@ -16,6 +16,28 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // Başlangıçta seçili olan sekme
+        onTap: (index) {
+          if (index == 0) {
+            // Ana sayfaya gitmek için yapılacak işlemler
+          } else if (index == 1) {
+            GetPage(
+                name: '/secondpage',
+                page: () => SecondPage()); // Üçüncü sayfayı aç
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Ana Sayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Üçüncü Sayfa',
+          ),
+        ],
+      ),
       appBar: AppBar(
         title: Text('Todo List'),
         actions: [
